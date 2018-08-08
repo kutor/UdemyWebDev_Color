1aecb9c861d2cf5ff2ff9d68c7c39025eceed70f
Ádám <nagy.adam.tamas@gmail.com>
2018-08-08 22:03:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let random = document.getElementById("random");
+
+
+function setGradient(){
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+
+function randomColors(){
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+    setGradient();
+}
+
+random.addEventListener("click", randomColors);
+
+
+
+
+
+window.onload = function(){
+    randomColors();
+    css.textContent = body.style.background + ";";
+};
